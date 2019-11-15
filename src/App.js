a0353db296e1d740d4a19010e30e007802497fba
Aaron-Doran <wesley.gossett@gmail.com>
2019-11-15 13:51:06 -0500

    Did not hit MVP. I got my data to display but need to add styling to page to hit MVP.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import WelcomePage from './components/WelcomePage';
+import CharacterList from ".//components/CharacterList";
+import {NavLink, Route} from "react-router-dom"
 
 export default function App() {
   return (
     <main>
+      <div>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/CharacterList">Character Cast</NavLink>
       <Header />
+      </div>
+      <Route exact path="/" component={WelcomePage}/>
+      <Route exact path="/CharacterList" component={CharacterList}/>
     </main>
+    
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <div className="cardWrapper">
+      <div className="card" key={props.id}>
+        <img src={props.character.image} alt='cast member of rick and morty' />
+        <h2>{props.character.name}</h2>
+        <p>{props.character.species}</p>
+        <p>{props.character.status}</p>
+      </div>
+    </div>
+  );
 }
